7f744fe0d7eaeef6e992f8542e988eaae1546fcd
tanvir14ahmed <tanvir14ahmed@gmail.com>
2026-03-11 04:37:28 +0600

    Added create_admin.py file

---
+++ create_admin.py
@@ -0,0 +1,21 @@
+import os
+import django
+
+# Set the environment to your project settings
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio_repo.settings')
+django.setup()
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+# CHOOSE YOUR DETAILS HERE
+username = 'tanvir14ahmed'
+email = 'tanvir14ahmed@gmail.com'
+password = 'Lifeis1*exam'
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password)
+    print(f"Superuser '{username}' created successfully!")
+else:
+    print(f"User '{username}' already exists.")
