814635bbeac70694838b30027eb71fb0f5a47b23
tanvir14ahmed <tanvir14ahmed@gmail.com>
2026-03-11 06:12:47 +0600

    Fix: Removed deploy-setup from urls to resolve AttributeError

--- main/urls.py
+++ main/urls.py
@@ -9,5 +9,4 @@ urlpatterns = [
     path('download-resume/', views.download_resume, name='download_resume'),
     path('blog/', views.blog_list, name='blog_list'),
     path('blog/<slug:slug>/', views.blog_detail, name='blog_detail'),
-    path('deploy-setup/', views.deploy_setup, name='deploy_setup'),
 ]
